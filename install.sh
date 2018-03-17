728cab9682037aeaf370603c7442f63da991f7b4
Ben Partridge <ben.partridge249@gmail.com>
2018-03-17 17:16:25 +1300

    Change git config to be configurable from template during install script

--- install.sh
+++ install.sh
@@ -3,24 +3,36 @@
 # Author: Ben Partridge
 
 # Set up tmux config
-./tmux/install.sh
+echo "Running tmux install script"
+echo "------------------------------------------"
+./tmux/install.sh || echo "Error while running tmux install script"
+echo ""
 
-# Symlink vimrc
-if [ ! -e ~/.vimrc ] ; then
-    ln -sf $(pwd)/vim/vimrc ~/.vimrc
-fi
+# Symlink vimrc -f will update link if it exists
+echo "Setting up Vim"
+echo "------------------------------------------"
+ln -sf $(pwd)/vim/vimrc ~/.vimrc || echo "Error creating vimrc symlink"
 # Install vim plugins
-vim +'PlugInstall --sync' +qa
+vim +'PlugInstall --sync' +qa || echo "Unable to install vim plugins"
 
 # Symlink nvim config
 if [ ! -d ~/.config/nvim ] ; then
     mkdir ~/.config/nvim
 fi
-if [ ! -e ~/.config/nvim/init.vim ] ; then
-    ln -sf $(pwd)/vim/nvimrc ~/.config/nvim/init.vim
-fi
+ln -sf $(pwd)/vim/nvimrc ~/.config/nvim/init.vim
+echo ""
+
+# Fill in git config template
+echo "Configuring git"
+echo "------------------------------------------"
+echo -e "\e[42mEnter your desired email to use in git config:\e[0m"
+read USER_EMAIL
+echo -e "\e[42mEnter your name for git config:\e[0m"
+read USER_NAME
+sed "s/\${git-email}/$USER_EMAIL/" ./git/gitconfig.templ |
+sed "s/\${git-name}/$USER_NAME/" > ./git/gitconfig
+echo "GIT CONFIG:"
+cat ./git/gitconfig
 
 # Symlink git config
-if [ ! -e ~/.gitconfig ] ; then
-    ln -sf $(pwd)/git/gitconfig ~/.gitconfig
-fi
+ln -sf $(pwd)/git/gitconfig ~/.gitconfig
