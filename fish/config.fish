02da82b3e43976cc66ed92efb70d7306bee52744
Ben Partridge <ben.partridge@immutable.com>
2023-02-02 09:31:55 +1100

    feat(fish): Adds fisher, nvm.fish and prompt colors settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,30 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    export COLORTERM="truecolor"
+    set -g fish_term24bit 1
 
-    export PATH=$PATH:$HOME/.local/bin
-    export PATH=$PATH:/usr/local/go/bin
-    export GOPATH=$HOME/go
-    export PATH=$PATH:$GOPATH/bin
+    set PATH $PATH $HOME/.local/bin
+    set PATH $PATH /usr/local/go/bin
+    set -x GOPATH $HOME/go
+    set PATH $PATH $GOPATH/bin
     # Set the GOPROXY environment variable
-    export GOPROXY=https://goproxy.io,direct
+    set -x GOPROXY https://goproxy.io,direct
     # Set environment variable allow bypassing the proxy for specified repos (optional)
-    export GOPRIVATE=github.com/immutable
-    export PATH=$PATH:$HOME/.cargo/bin
+    set -x GOPRIVATE github.com/immutable
+    set PATH $PATH $HOME/.cargo/bin
     
     # Start docker
-    if docker ps 2>&1 >> /dev/null; then
+    if docker ps 2>&1 >> /dev/null
     else
         echo "Starting docker server..."
         sudo service docker start
-    fi
+    end
 
-    # NVM setup
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion"  ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    NVM_DEFAULT="16"
+    set NVM_DEFAULT "lts"
     nvm use $NVM_DEFAULT
+
+    # Prompt
+    set -g hydro_color_error red
+    set -g hydro_color_pwd magenta
+    set -g hydro_clor_git yellow
+    set -g hydro_color_prompt green
 end
